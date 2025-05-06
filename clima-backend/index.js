e7c803067750346418867fec4024dd4210a23f3f
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-05-05 21:30:16 -0400

    App vite + express juntas

--- clima-backend/index.js
+++ clima-backend/index.js
@@ -3,11 +3,17 @@ const cors = require('cors');
 require('dotenv').config();
 const weatherRoutes = require('./routes/weather');
 const cityRoutes = require('./routes/city')
-
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use(cors());
 app.use('/api/weather', weatherRoutes);
 app.use('/api/city', cityRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
